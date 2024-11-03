eb091a5166c1e8d43d03c889b91998c6a9333297
abyankamal <abyankamal8@gmail.com>
2024-11-03 11:29:27 +0700

    add tailwind

--- src/components/MovieSearch.vue
+++ src/components/MovieSearch.vue
@@ -5,7 +5,7 @@
       type="text"
       placeholder="Search for a movie..."
       @keyup.enter="emitSearch"
-      class="border border-gray-300 p-2 rounded w-full"
+      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
     />
     <button @click="emitSearch" class="mt-2 bg-blue-500 text-white p-2 rounded">
       Search
